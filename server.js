be6d8238f8f4364dadc0f90fefd5c83d1102ce2f
VINEET KUMAR VERMA <103252793+vineetvermaa61@users.noreply.github.com>
2023-07-21 13:28:15 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const colors = require('colors')
+const morgan = require('morgan')
+const dotenv = require('dotenv');
+const connectDB = require('./config/db');
+
+// doenv config
+dotenv.config();
+
+// mongodb connection
+connectDB();
+
+// rest object ke liye variable
+const app = express()
+
+// middlewares 
+app.use(express.json());
+app.use(morgan("dev"));
+
+// routes
+app.use("/api/v1/user", require("./routes/userRoutes"));
+
+// port
+const port = process.env.PORT||8080
+// listen port
+app.listen(port, ()=>{
+    console.log(`Server Running in ${process.env.NODE_MODE} Mode on port ${process.env.PORT}`.bgCyan.white );
+});
+
+
+
+
+
+
+
+
